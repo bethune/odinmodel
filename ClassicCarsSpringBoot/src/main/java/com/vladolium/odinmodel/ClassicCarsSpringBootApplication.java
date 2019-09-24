4aa4f745ac99ca2fc3953f6b364d38c0c4d35bcb
vladolium <vladolium@gmail.com>
2019-09-24 13:30:20 +0200

    Project ClassicCarsSpringBoot created

---
+++ ClassicCarsSpringBoot/src/main/java/com/vladolium/odinmodel/ClassicCarsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vladolium.odinmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicCarsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicCarsSpringBootApplication.class, args);
+	}
+
+}
