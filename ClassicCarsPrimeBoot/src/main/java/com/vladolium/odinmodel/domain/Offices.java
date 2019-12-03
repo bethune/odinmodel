d7ee251ca2c7865c143bb62b800d74b798eb1dc9
wolfadler <vukjeorao@gmail.com>
2019-12-03 23:59:29 +0100

    Started primeboot generator

---
+++ ClassicCarsPrimeBoot/src/main/java/com/vladolium/odinmodel/domain/Offices.java
@@ -0,0 +1,65 @@
+package com.vladolium.odinmodel.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.*;
+
+@Entity
+public class Offices extends ContactDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	@NotNull
+	@Size(max=10)
+	private String territory;
+	
+	public String getTerritory() {
+		return territory;
+	}
+	public void setTerritory(String territory) {
+		this.territory = territory;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+//Code between start and end will not be removed during generation.
+//Start of user code for this inheriting entity
+//End of user code
+}
