df706e6ebb4076513bbc7594049320df47578684
wolfadler <vukjeorao@gmail.com>
2019-12-16 00:06:42 +0100

    Started service in javaee generator

---
+++ ClassicCarsJavaEE8/src/main/java/com/vladolium/odinmodel/service/OrdersService.java
@@ -0,0 +1,25 @@
+package com.vladolium.odinmodel.service;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+//import com.vladolium.odinmodel.domain.*;
+//import java.time.*;
+//import java.util.*;
+//import org.springframework.data.domain.*;
+//import com.vladolium.odinmodel.domain.*;
+//import com.vladolium.odinmodel.domain.Orders;
+//import com.vladolium.odinmodel.domain.Orders.*;
+//import com.vladolium.odinmodel.wrapper.*;
+
+@Stateless
+public class OrdersService {
+
+ 	@PersistenceContext
+    private EntityManager em;
+
+
+//Code between start and end will not be removed during generation.
+//Start of user code for this service
+//End of user code
+}
---
+++ ClassicCarsJavaEE8/src/main/java/com/vladolium/odinmodel/service/ReviewsService.java
@@ -0,0 +1,25 @@
+package com.vladolium.odinmodel.service;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+//import com.vladolium.odinmodel.domain.*;
+//import java.time.*;
+//import java.util.*;
+//import org.springframework.data.domain.*;
+//import com.vladolium.odinmodel.domain.*;
+//import com.vladolium.odinmodel.domain.Reviews;
+//import com.vladolium.odinmodel.domain.Reviews.*;
+//import com.vladolium.odinmodel.wrapper.*;
+
+@Stateless
+public class ReviewsService {
+
+ 	@PersistenceContext
+    private EntityManager em;
+
+
+//Code between start and end will not be removed during generation.
+//Start of user code for this service
+//End of user code
+}
