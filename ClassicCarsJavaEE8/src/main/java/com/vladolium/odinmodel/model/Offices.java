29c7181ae263928a93fe70dc66f893fe5b0cf8a8
wolfadler <vukjeorao@gmail.com>
2019-12-15 18:28:42 +0100

    Started JavaEE8 generator
    Test project ClassicCarsJavaEE8 created
    
    Model generator finished

---
+++ ClassicCarsJavaEE8/src/main/java/com/vladolium/odinmodel/model/Offices.java
@@ -0,0 +1,63 @@
+package com.vladolium.odinmodel.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.*;
+import java.util.*;
+
+@Entity
+public class Offices extends ContactDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	@NotNull
+	@Size(max=10)
+	private String territory;
+	
+	public String getTerritory() {
+		return territory;
+	}
+	public void setTerritory(String territory) {
+		this.territory = territory;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+//Code between start and end will not be removed during generation.
+//Start of user code for this inheriting entity
+//End of user code
+}
---
+++ ClassicCarsJavaEE8/src/main/java/com/vladolium/odinmodel/model/Reviews.java
@@ -0,0 +1,105 @@
+package com.vladolium.odinmodel.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.*;
+import java.util.*;
+
+@Entity
+public class Reviews {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@NotNull
+	@Size(max=4000)
+	private String reviewText;
+	
+	public String getReviewText() {
+		return reviewText;
+	}
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@NotNull
+	private LocalDate reviewDate;
+	
+	public LocalDate getReviewDate() {
+		return reviewDate;
+	}
+	public void setReviewDate(LocalDate reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+	
+	
+	
+	
+	
+	
+	@NotNull
+	private LocalTime reviewTime;
+	
+	public LocalTime getReviewTime() {
+		return reviewTime;
+	}
+	public void setReviewTime(LocalTime reviewTime) {
+		this.reviewTime = reviewTime;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+//Code between start and end will not be removed during generation.
+//Start of user code for this entity
+//End of user code
+}
