480c285fdaf43930c7ed876490e199c5ac1f378a
wolfadler <vukjeorao@gmail.com>
2020-02-23 22:42:49 +0100

    jpa gotov do veza

---
+++ ClassicCarsSpringBoot/src/main/java/com/vladolium/odinmodel/model/Customers.java
@@ -0,0 +1,17 @@
+package com.vladolium.odinmodel.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.*;
+
+@Entity
+public class Customers extends ContactDetails {
+
+		
+//Code between start and end will not be removed during generation.
+//Start of user code for this inheriting entity
+//End of user code
+}
---
+++ ClassicCarsSpringBoot/src/main/java/com/vladolium/odinmodel/model/ProductLines.java
@@ -0,0 +1,17 @@
+package com.vladolium.odinmodel.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.*;
+
+@Entity
+public class ProductLines {
+
+
+//Code between start and end will not be removed during generation.
+//Start of user code for this entity
+//End of user code
+}
