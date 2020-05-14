e180bb9040ec731a3483efd3e169eadb590ca395
ranko t zveran <ranko t zveran@DESKTOP-L7L05N3>
2020-05-14 14:57:11 +0200

    gotov spring boot querydsl pristup
    ostaje jooq i da vidim verzije spring boota, kao i lombok na novijoj
    verziji

--- ClassicCarsSpringBoot/src/main/java/com/vladolium/odinmodel/controllers/CustomersController.java
+++ ClassicCarsSpringBoot/src/main/java/com/vladolium/odinmodel/controllers/CustomersController.java
@@ -35,19 +35,20 @@ public class CustomersController {
 	public void deleteOneById(@PathVariable Long id) {
 		customersInterface.deleteOneById(id);
 	}
-
+	
 	@GetMapping("/{id}")
 	public Customers readOneById(@PathVariable Long id) {
 		return customersInterface.readOneById(id);
 	}
-
+	
 	@PutMapping("/{id}")
 	public Customers updateOneById(@PathVariable Long id, @RequestBody Customers customers) {
-
+		
 		Customers current = customersInterface.readOneById(id);
-
+			
 		current.setEmployees(customers.getEmployees());
-
+		
+		
 		current.setReviewsList(customers.getReviewsList());
 		current.setCity(customers.getCity());
 		current.setPhone(customers.getPhone());
@@ -60,7 +61,7 @@ public class CustomersController {
 		current.setLastName(customers.getLastName());
 		current.setState(customers.getState());
 		current.setPostalCode(customers.getPostalCode());
-
+				
 		return customersInterface.saveOne(current);
 	}
 
@@ -68,98 +69,198 @@ public class CustomersController {
 	public Iterable<Customers> readAll() {
 		return customersInterface.readAll();
 	}
-
+	
 	@GetMapping("/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Customers> readAllPagination(@PathVariable Integer pageNumber, @PathVariable Integer perPageNumber) {
+	public Page<Customers> readAllPagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
 		return customersInterface.readAllPagination(page);
 	}
 
 	@GetMapping("/search")
-	public Iterable<Customers> search(Long employeesId,
-
-			List<Reviews> reviewsList, String city, String phone, String addressLine2, String firstName,
-			Double creditLimit, String customerName, String addressLine1, String country, String lastName, String state,
-			String postalCode) {
-		return customersInterface.search(employeesId,
-
-				reviewsList, city, phone, addressLine2, firstName, creditLimit, customerName, addressLine1, country,
-				lastName, state, postalCode);
+	public Iterable<Customers> search(
+		Long employeesId,
+		
+		
+		List<Reviews> reviewsList,
+		String city,
+		String phone,
+		String addressLine2,
+		String firstName,
+		Double creditLimit,
+		String customerName,
+		String addressLine1,
+		String country,
+		String lastName,
+		String state,
+		String postalCode
+	) {
+		return customersInterface.search(
+			employeesId,
+			
+			
+			reviewsList,
+			city,
+			phone,
+			addressLine2,
+			firstName,
+			creditLimit,
+			customerName,
+			addressLine1,
+			country,
+			lastName,
+			state,
+			postalCode
+		);
 	}
-
+	
 	@GetMapping("/search/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Customers> searchPagination(@PathVariable Integer pageNumber, @PathVariable Integer perPageNumber,
-			Long employeesId,
-
-			List<Reviews> reviewsList, String city, String phone, String addressLine2, String firstName,
-			Double creditLimit, String customerName, String addressLine1, String country, String lastName, String state,
-			String postalCode) {
+	public Page<Customers> searchPagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber,
+		Long employeesId,
+		
+		
+		List<Reviews> reviewsList,
+		String city,
+		String phone,
+		String addressLine2,
+		String firstName,
+		Double creditLimit,
+		String customerName,
+		String addressLine1,
+		String country,
+		String lastName,
+		String state,
+		String postalCode
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
-
-		return customersInterface.searchPagination(page, employeesId,
-
-				reviewsList, city, phone, addressLine2, firstName, creditLimit, customerName, addressLine1, country,
-				lastName, state, postalCode);
+	
+		return customersInterface.searchPagination(
+			page,
+			employeesId,
+			
+			
+			reviewsList,
+			city,
+			phone,
+			addressLine2,
+			firstName,
+			creditLimit,
+			customerName,
+			addressLine1,
+			country,
+			lastName,
+			state,
+			postalCode
+		);
 	}
 
 	private OrdersInterface ordersInterface;
-
+	
 	@Autowired
 	public void setOrdersInterface(OrdersInterface ordersInterface) {
 		this.ordersInterface = ordersInterface;
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping("/{customersId}/orders")
 	public Iterable<Orders> readAllOrdersByCustomersId(@PathVariable Long customersId) {
 		return ordersInterface.readAllByCustomersId(customersId);
 	}
-
+	
 	@GetMapping("/{customersId}/orders/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Orders> readAllOrdersByCustomersIdPagination(@PathVariable Integer pageNumber,
-			@PathVariable Integer perPageNumber, @PathVariable Long customersId) {
+	public Page<Orders> readAllOrdersByCustomersIdPagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber,
+		@PathVariable Long customersId
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
 		return ordersInterface.readAllByCustomersId(customersId, page);
 	}
-
+	
+	
 	@GetMapping("/customerName={customersCustomerName}/orders")
 	public Iterable<Orders> readAllOrdersByCustomersCustomerName(@PathVariable String customersCustomerName) {
 		return ordersInterface.readAllByCustomersCustomerName(customersCustomerName);
 	}
-
+	
 	@GetMapping("/customerName={customersCustomerName}/orders/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Orders> readAllOrdersByCustomersCustomerNamePagination(@PathVariable Integer pageNumber,
-			@PathVariable Integer perPageNumber, @PathVariable String customersCustomerName) {
+	public Page<Orders> readAllOrdersByCustomersCustomerNamePagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber,
+		@PathVariable String customersCustomerName
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
 		return ordersInterface.readAllByCustomersCustomerName(customersCustomerName, page);
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private PaymentsInterface paymentsInterface;
-
+	
 	@Autowired
 	public void setPaymentsInterface(PaymentsInterface paymentsInterface) {
 		this.paymentsInterface = paymentsInterface;
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping("/{customersId}/payments")
 	public Iterable<Payments> readAllPaymentsByCustomersId(@PathVariable Long customersId) {
 		return paymentsInterface.readAllByCustomersId(customersId);
 	}
-
+	
 	@GetMapping("/{customersId}/payments/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Payments> readAllPaymentsByCustomersIdPagination(@PathVariable Integer pageNumber,
-			@PathVariable Integer perPageNumber, @PathVariable Long customersId) {
+	public Page<Payments> readAllPaymentsByCustomersIdPagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber,
+		@PathVariable Long customersId
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
 		return paymentsInterface.readAllByCustomersId(customersId, page);
 	}
-
+	
+	
 	@GetMapping("/customerName={customersCustomerName}/payments")
 	public Iterable<Payments> readAllPaymentsByCustomersCustomerName(@PathVariable String customersCustomerName) {
 		return paymentsInterface.readAllByCustomersCustomerName(customersCustomerName);
 	}
-
+	
 	@GetMapping("/customerName={customersCustomerName}/payments/page={pageNumber}/perPage={perPageNumber}")
-	public Page<Payments> readAllPaymentsByCustomersCustomerNamePagination(@PathVariable Integer pageNumber,
-			@PathVariable Integer perPageNumber, @PathVariable String customersCustomerName) {
+	public Page<Payments> readAllPaymentsByCustomersCustomerNamePagination(
+		@PathVariable Integer pageNumber,
+		@PathVariable Integer perPageNumber,
+		@PathVariable String customersCustomerName
+	) {
 		Pageable page = PageRequest.of(pageNumber, perPageNumber);
 		return paymentsInterface.readAllByCustomersCustomerName(customersCustomerName, page);
 	}
@@ -168,14 +269,15 @@ public class CustomersController {
 	public Customers readOneByCustomerName(@PathVariable String customerName) {
 		return customersInterface.readOneByCustomerName(customerName);
 	}
-
+	
 	@PutMapping("/{customerName}")
 	public Customers updateOneByCustomerName(@PathVariable String customerName, @RequestBody Customers customers) {
-
+		
 		Customers current = customersInterface.readOneByCustomerName(customerName);
-
+			
 		current.setEmployees(customers.getEmployees());
-
+		
+		
 		current.setReviewsList(customers.getReviewsList());
 		current.setCity(customers.getCity());
 		current.setPhone(customers.getPhone());
@@ -188,9 +290,11 @@ public class CustomersController {
 		current.setLastName(customers.getLastName());
 		current.setState(customers.getState());
 		current.setPostalCode(customers.getPostalCode());
-
+				
 		return customersInterface.saveOne(current);
 	}
+
+	
 
 //Code between start and end will not be removed during generation.
 //Start of user code for this controller
